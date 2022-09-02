84c7436d5ce278f619ca42302297fd0435128d7c
Khoa LTN <90553559+SteveKhoa@users.noreply.github.com>
2022-09-02 16:19:27 +0700

    Update ready.cpp

--- ready.cpp
+++ ready.cpp
@@ -4,21 +4,6 @@
 
 // https://en.wikipedia.org/wiki/Netpbm
 
-// TO-DO:
-//  *IMPORTANT* Implement method rotate() :
-//  Rotate image by 45, 90 or alpha degree
-//  Pixel averaging: Loop for every 3x3 block, replace all its pixels with their average
-//  Scaling up & down images
-//  https://www.youtube.com/watch?v=rLMznzIslVA
-
-// Benchmarking : try to optimize time (as i see, it is a bit slow isn't it)
-
-// Really good practice materials:
-// https://www.usna.edu/Users/cs/wcbrown/courses/F14IC210/lab/l09/lab.html
-
-// JPEG
-//  Take a look at JPEG TAKEAWAY.pptx
-
 using namespace dip;
 
 int main()
